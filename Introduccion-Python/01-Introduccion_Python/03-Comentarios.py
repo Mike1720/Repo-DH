871174b3c2c498eb0dd34ff436d403fa6a4eeaf7
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-08-22 20:15:33 -0600

    Comentarios en Python

---
+++ Introduccion-Python/01-Introduccion_Python/03-Comentarios.py
@@ -0,0 +1,22 @@
+# Esto va a ser un comentario de una sola linea
+
+"""
+    Esto es un comentario
+    de varias lineas.
+    Ideal para escribir explicaciones largas
+    o indicaciones sobre el código
+"""
+
+# Los comentarios son ignorados por el intérprete de Python
+# y no afectan la ejecución del programa.
+
+
+num1 = 7;
+num2 = 10;
+
+# Comentario entre lineas de código
+
+resultado = num1 + num2  # Suma de dos números
+print(resultado) # imprime el resultado de la suma
+
+# Comentario al final del código
