65b04f439437559e0054adb9666fa890eb93b27e
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-08-27 20:03:31 -0600

    Operadores de pertenencia

---
+++ Introduccion-Python/01-Introduccion_Python/09-Operadores_de_pertenencia.py
@@ -0,0 +1,12 @@
+# Operadores de pertenencia
+# Los operadores de pertenencia se utilizan para verificar si un valor o variable está presente en una secuencia (como una lista, tupla, conjunto o cadena).
+# Los dos operadores de pertenencia en Python son "in" y "not in".
+
+# Operador "in"
+txt = "Listado de tecnologías de programación: Python, Java, C++, JavaScript"
+print("Python" in txt) # Devuelve True ya que "Python" está en la cadena
+print("Ruby" in txt) # Devuelve False ya que "Ruby" no está en la cadena
+
+# Operador "not in"
+print("Python" not in txt) # Devuelve False ya que "Python" se encuentra en la cadena
+print("Ruby" not in txt) # Devuelve True ya que "Ruby" no se encuentra en la cadena
