2fd93ba7b1d4e5bc6c4a1e1e0e2a95c2c0216c23
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-08-28 20:28:36 -0600

    Estructura de control for

---
+++ Introduccion-Python/02-Logica_y_estructuras_de_control/08-Estructura_de_control_for.py
@@ -0,0 +1,11 @@
+# Estructura de control for
+# La estructura de control for se utiliza para iterar sobre una secuencia (como una lista, una tupla, un diccionario, un conjunto o una cadena de texto) o sobre un rango de números. Permite ejecutar un bloque de código varias veces, una por cada elemento en la secuencia o rango.
+
+for i in range(5):  # Itera desde 0 hasta 4
+    print(f"Iteración número: {i}")
+
+for i in range(1, 11):  # Itera desde 1 hasta 10
+    print(f"Número: {i}")
+
+for i in range(0, 21, 2):  # Itera desde 0 hasta 20, en pasos de 2
+    print(f"Número actual: {i}")
