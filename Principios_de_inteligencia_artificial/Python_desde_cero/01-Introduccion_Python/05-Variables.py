1efda50b2239076139eabd5cc515516872b9e401
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-09-01 20:32:01 -0600

    Variables en Python

---
+++ Principios_de_inteligencia_artificial/Python_desde_cero/01-Introduccion_Python/05-Variables.py
@@ -0,0 +1,30 @@
+# Variables
+"""
+    Las variables son espacios de memoria que se utilizan para almacenar datos.
+    En Python no es necesario declarar el tipo de variable, debido a que es un 
+        lenguaje de tipado dinamico.
+    Cada variable tiene un nombre unico y un valor asociado.
+    Para asignar un valor a una variable se utiliza el operador de asignación (=).
+    Existen convenciones para nombrar variables:
+    - No pueden COMENZAR con números.
+    - No pueden contener espacios.
+    - No pueden contener carácteres especiales, excepto guión bajo (_).
+    - No pueden ser palabras reservadas del lenguaje.
+    - Se recomienda utilizar nombres descriptivos.
+    - Se recomienda utilizar convenciones de nomenclatura como:
+        - snake_case: palabras separadas por guiones bajos
+        - camelCase: primera palabra en minúscula y las siguientes en mayúscula.
+        - PascalCase: todas las palabras en mayúscula.
+        - SCREAMING_SNAKE_CASE: todas las palabras en mayúscula y separadas por guines bajos.
+"""
+
+numero = 20
+numero2 = 30
+texto = "Hola mundo"
+HOLA_MUNDO = "HOLA MUNDO!"
+_variableX = "Variable random"
+
+
+print(numero)
+print(texto)
+print(_variableX)
