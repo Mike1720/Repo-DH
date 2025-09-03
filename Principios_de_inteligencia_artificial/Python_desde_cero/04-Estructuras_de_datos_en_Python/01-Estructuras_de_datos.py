3c05873a1fa262863aa8b4979cfd90be57dd91d2
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-09-02 20:15:04 -0600

    Estructuras de datos

---
+++ Principios_de_inteligencia_artificial/Python_desde_cero/04-Estructuras_de_datos_en_Python/01-Estructuras_de_datos.py
@@ -0,0 +1,49 @@
+# ----------------------
+# TIPOS DE DATOS SECUENCIA/COLECCIÓN
+# ----------------------
+
+# list (listas) - colección ordenada y mutable
+lista = [1, 2, 3, 4, 5]
+
+# tuple (tuplas) - colección ordenada e inmutable
+tupla = (1, 2, 3, 4, 5)
+tupla_sin_parentesis = 1, 2, 3, 4, 5  # también es una tupla
+
+# range (rango) - colección ordenada e inmutable de números enteros
+rango = range(0, 10)  # Secuencia de números del 0 al 9
+
+# ----------------------
+# TIPO DE DATO DE MAPEO (MAPPING TYPE)
+# ----------------------
+
+# dict (diccionarios) - colección no ordenada de pares clave-valor
+diccionario = {
+    "nombre": "Miguel",
+    "edad": 25,
+    "tecnologias": ["Python", "JavaScript", "HTML", "CSS"],
+}
+
+# ----------------------
+# TIPOS DE DATOS DE CONJUNTO (SET TYPES)
+# ----------------------
+
+# set (conjuntos) - colección no ordenada y mutable de elemtentos únicos
+conjunto = {1, 2, 3, 4, 5}
+
+# ----------------------
+# TIPOS DE DATO NULO
+# ----------------------
+
+# NoneType - representa la ausencia de valor
+nulo = None
+
+
+# ----------------------
+# STRINGS (CADENAS DE CARACTERES)
+# ----------------------
+string_comillas_simples = 'Hola, mundo!'
+string_comllas_dobles = "Hola, mundo!"
+string_comillas_simples_triples = '''Hola,
+mundo!'''
+string_comillas_dobles_triples = """Hola,
+mundo!"""
