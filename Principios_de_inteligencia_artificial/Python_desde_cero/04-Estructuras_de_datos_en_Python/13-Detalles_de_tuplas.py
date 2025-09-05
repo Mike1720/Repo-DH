a7b4383309f6031194de6ddd138df567b850e4ad
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-09-04 20:21:32 -0600

    Detalles de tuplas

---
+++ Principios_de_inteligencia_artificial/Python_desde_cero/04-Estructuras_de_datos_en_Python/13-Detalles_de_tuplas.py
@@ -0,0 +1,12 @@
+frutas1 = ("Manzana", "Uva", "Fresa", "Mandarina")
+frutas2 = ("Aguacate", "Naranja", "Kiwi")
+
+
+# MULTIPLICA LOS ELEMENTOS LA CANTIDAD INDICADA
+# frutas = frutas1 * 2 + frutas2
+# print(frutas.count("Manzana"))
+# print(frutas)
+
+# VISUALIZAR ÍNDICE
+frutas1 += frutas2
+print(frutas1.index("Kiwi"))
