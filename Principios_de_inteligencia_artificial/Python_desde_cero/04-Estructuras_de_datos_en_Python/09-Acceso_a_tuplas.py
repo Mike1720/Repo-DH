a6e0083978024cbf796c5dd5e3152c2f69d5c39d
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-09-04 19:44:28 -0600

    Acceso a tuplas

---
+++ Principios_de_inteligencia_artificial/Python_desde_cero/04-Estructuras_de_datos_en_Python/09-Acceso_a_tuplas.py
@@ -0,0 +1,9 @@
+# index =      0        1       2          3           4           5        6
+frutas = ("Manzana", "Uva", "Fresa", "Mandarina", "Aguacate", "Naranja", "Kiwi")
+# -index =    -7       -6      -5         -4          -3          -2       -1
+print(frutas[1])
+print(frutas[1:4])
+print(frutas[-7:-1])
+
+if "Mandarina" in frutas:
+    print("Si hay mandarina en las frutas que tengo")
