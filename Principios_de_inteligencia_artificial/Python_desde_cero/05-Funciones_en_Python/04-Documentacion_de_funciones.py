b2f23366cd87dc9a1105804053055902a519402b
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-09-09 20:24:06 -0600

    Documentacion de funciones

---
+++ Principios_de_inteligencia_artificial/Python_desde_cero/05-Funciones_en_Python/04-Documentacion_de_funciones.py
@@ -0,0 +1,19 @@
+def suma(a, b):
+    """Devuelve la suma de dos números
+
+    Args:
+        a (int): Primer número a sumar
+        b (int): Segundo número a sumar
+
+    Returns:
+        int: Suma de a y b
+    """
+    return a + b
+
+
+resultado = suma(a=4, b=10)
+print(resultado)
+
+# VISUALIZAR DOCUMENTACIÓN DE FUNCIÓN POR CONSOLA
+# help(suma)
+# print(suma.__doc__)
